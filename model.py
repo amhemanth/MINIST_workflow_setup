4ee6547835289b82d1ef89b0239f25fa5266f7fb
A.M. Hemanth <amhemanth1999@gmail.com>
2025-03-26 15:33:09 +0530

    Modified the model architecture to reduce the number of parameters

--- model.py
+++ model.py
@@ -5,25 +5,25 @@ class MNISTModel(nn.Module):
     def __init__(self):
         super(MNISTModel, self).__init__()
         
-        # First convolutional layer
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
+        # First convolutional layer - reduced filters from 16 to 8
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, padding=1)
         self.relu1 = nn.ReLU()
         self.pool1 = nn.MaxPool2d(2)
         
-        # Second convolutional layer
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        # Second convolutional layer - reduced filters from 32 to 16
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
         self.relu2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d(2)
         
-        # Fully connected layers
-        self.fc1 = nn.Linear(32 * 7 * 7, 128)
+        # Fully connected layers - reduced hidden units from 128 to 64
+        self.fc1 = nn.Linear(16 * 7 * 7, 64)
         self.relu3 = nn.ReLU()
-        self.fc2 = nn.Linear(128, 10)
+        self.fc2 = nn.Linear(64, 10)
 
     def forward(self, x):
         x = self.pool1(self.relu1(self.conv1(x)))
         x = self.pool2(self.relu2(self.conv2(x)))
-        x = x.view(-1, 32 * 7 * 7)
+        x = x.view(-1, 16 * 7 * 7)
         x = self.relu3(self.fc1(x))
         x = self.fc2(x)
         return x
